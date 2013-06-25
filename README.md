b9fb30895b2df0ccae1db6d0f71dc0ee76cc2d02
tokubass <tomi21110@gmail.com>
2013-06-26 01:58:11 +0900

    init

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-p5-EPUB-Filter
-==============
+# NAME
 
-edit EPUB
+EPUB::Filter - It's new $module
+
+# SYNOPSIS
+
+    use EPUB::Filter;
+
+# DESCRIPTION
+
+EPUB::Filter is ...
+
+# LICENSE
+
+Copyright (C) tokubass.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tokubass <tomi21110@gmail.com>
---
+++ lib/EPUB/Filter.pm
@@ -0,0 +1,39 @@
+package EPUB::Filter;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+EPUB::Filter - It's new $module
+
+=head1 SYNOPSIS
+
+    use EPUB::Filter;
+
+=head1 DESCRIPTION
+
+EPUB::Filter is ...
+
+=head1 LICENSE
+
+Copyright (C) tokubass.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokubass E<lt>tomi21110@gmail.comE<gt>
+
+=cut
+
